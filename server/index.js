f6ea50f2f3a2001b6da3e43d079c586c76b7bc0c
tstrat <tstrat@email.arizona.edu>
2018-10-12 10:36:22 -0700

    Initialize massive with env files

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const controller = require('./controller');
+const massive = require('massive');
+require('dotenv').config();
 
 
 const app = express();
 app.use(bodyParser.json());
 
+massive(process.env.CONNECTION_STRING)
+.then(database => {
+    app.set('db', database);
+}) .catch( error => console.error("Trouble setting up connection to postgreSQL:", error));
 
-const SERVER_PORT = 4000;
+
+const SERVER_PORT = process.env.PORT || 4000;
 app.listen(SERVER_PORT, () => console.log("Listening on port:", SERVER_PORT));
