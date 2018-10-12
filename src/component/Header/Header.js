2623ad31851ec3503d0c9d40a20db6da14b6b650
tstrat <tstrat@email.arizona.edu>
2018-10-12 15:29:16 -0700

    Getting ready for migration to routes

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,12 +1,15 @@
 import React from "react"
 import './header.css';
 import shelfieIcon from '../../media/shelfie_icon.png';
+import { NavLink } from 'react-router-dom';
 
 export default function Header(props)  {
     return (
         <div className="header">
             <img alt="Shelfie Logo" src={shelfieIcon}/>
             <h1>SHELFIE</h1>
+            <NavLink to="/">Dashboard</NavLink>
+            <NavLink to="/">Add Inventory</NavLink>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './reset.css';
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, document.getElementById('root'));
 
 // serviceWorker.unregister();
