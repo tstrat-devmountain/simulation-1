07854a8c4c1e3685e4839a6418b90fb985fa85e6
tstrat <tstrat@email.arizona.edu>
2018-10-12 11:05:35 -0700

    Fleshed out Header starting point

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Dashboard />
         <Form />
-        <Header />
+        
       </div>
     );
   }
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,12 @@
 import React from "react"
-
+import './header.css';
+import shelfieIcon from '../../media/shelfie_icon.png';
 
 export default function Header(props)  {
     return (
-        <div>
-            Product
+        <div className="header">
+            <img src={shelfieIcon}/>
+            <h1>SHELFIE</h1>
         </div>
     )
 }
